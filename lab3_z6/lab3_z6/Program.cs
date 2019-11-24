e3be8f3d330ade5785c07ce197686403c8ddad72
murmiloo <kkaplyta@wp.pl>
2019-11-24 16:25:16 +0100

    Create Program.cs

---
+++ lab3_z6/lab3_z6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_z6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "111";
+
+            while (data != "-1")
+            {
+                Console.WriteLine("Podaj napis");
+                data = Console.ReadLine();
+                if (data != "-1")
+                {
+                    for (int i = 1; i < data.Length; i = i + 2)
+                        data = data.Insert(i, " ");
+                    Console.WriteLine(data);
+                }
+
+            }
+        }
+    }
+}
