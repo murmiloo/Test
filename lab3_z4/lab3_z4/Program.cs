99994e9d2c6c1e871a979a766e2d420e0d799dc6
murmiloo <kkaplyta@wp.pl>
2019-11-24 16:21:42 +0100

    Create Program.cs

---
+++ lab3_z4/lab3_z4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+enum Day { Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7};
+
+namespace lab3_z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "111";
+            int flaga;
+            while (data != "-1")
+            {
+                Console.WriteLine("Podaj numer odpowiadajacy dniu tygodnia:");
+                data = Console.ReadLine();
+                if (data != "-1")
+                {
+                    int.TryParse(data, out flaga);
+                    if (flaga > 0 && flaga < 8)
+                    {
+                        Console.WriteLine("{0}", Enum.GetName(typeof(Day), flaga));
+                    }
+                    else
+                        Console.WriteLine("Zly numer");
+                }
+            }
+        }
+    }
+}
