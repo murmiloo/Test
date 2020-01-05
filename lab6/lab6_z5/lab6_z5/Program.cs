bea6bd432a1385938838c9039749da2f9ecea4e5
murmiloo <kkaplyta@wp.pl>
2020-01-05 20:08:32 +0100

    Create Program.cs

---
+++ lab6/lab6_z5/lab6_z5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_z5
+{
+    abstract class Home
+    {
+        public Home(int pr)
+        {
+            price = pr;
+        }
+        public int price;
+        abstract public int Price();
+    }
+
+    class Home2 : Home
+    {
+        public Home2(int pr) : base(pr) { }
+        public override int Price()
+        {
+            return price;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie5");
+        }
+    }
+}
