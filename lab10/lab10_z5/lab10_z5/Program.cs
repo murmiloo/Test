059804b2977fe71e1b55c31416aa5c7927fa5e66
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:33:47 +0100

    Create Program.cs

---
+++ lab10/lab10_z5/lab10_z5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab10_z5
+{
+    class Program
+    {
+        static readonly object locker = new object();
+        static void Main(string[] args)
+        {
+            new Thread(Run).Start();
+            Run();
+        }
+        static void Run()
+        {
+            lock (locker)
+            {
+                //exclusive lock
+            }
+        }
+    }
+}
