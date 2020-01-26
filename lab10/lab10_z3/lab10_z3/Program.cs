7442256d93ab462474dd168fee9b3f4551a701b9
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:33:35 +0100

    Create Program.cs

---
+++ lab10/lab10_z3/lab10_z3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab10_z3
+{
+    class ThreadTest
+    {
+        bool done;
+        static void Main()
+        {
+            ThreadTest test = new ThreadTest();
+            new Thread(test.Run).Start();
+            test.Run();
+            Console.ReadLine();
+        }
+        // Zauważ, że Run jest teraz metodą instancji
+        void Run()
+        {
+            if (!done) { done = true; Console.WriteLine("Done"); }
+        }
+    }
+}
+
+//output: Done wypisal sie 1 raz
