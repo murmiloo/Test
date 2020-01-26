ae4c181caf2103d62fe7f858cd3be0564c93a250
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:34:03 +0100

    Create Program.cs

---
+++ lab11/lab11_z1/lab11_z1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab11_z1
+{
+    class Program
+    {
+        static void Run() { throw null; } // Throws a NullReferenceException
+        public static void Main()
+        {
+            try
+            {
+                new Thread(Run).Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!");
+            }
+        }
+    }
+}
+// System.NullReferenceException: 'Odwołanie do obiektu nie zostało ustawione na wystąpienie obiektu.'
+
