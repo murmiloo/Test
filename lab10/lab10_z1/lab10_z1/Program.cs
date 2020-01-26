e0e55f07037d4b3317914b86b82634e086809f1c
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:33:18 +0100

    Create Program.cs

---
+++ lab10/lab10_z1/lab10_z1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab10_z1
+{
+    class ThreadExample
+    {
+        static void Main()
+        {
+            Thread t = new Thread(Write1); //Uruchom inny wątek
+            t.Start();
+            // Główny wątek.
+            for (int i = 0; i < 1000; i++) Console.Write("0");
+            Console.ReadLine();
+        }
+        //Inny wątek
+        static void Write1()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("1");
+        }
+    }
+}
