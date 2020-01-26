348dded6b79c48191b4b3849c56619c971e6be01
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:34:20 +0100

    Create Program.cs

---
+++ lab11/lab11_z3/lab11_z3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11_z3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task.Factory.StartNew(Run);
+            Console.ReadLine();
+        }
+        static void Run()
+        {
+            Console.WriteLine("Hello !! The thread pool!");
+        }
+    }
+}
