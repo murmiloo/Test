deb09f99bf41652b42e8aefa6cb98d7ddcfce76d
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:34:35 +0100

    Create Program.cs

---
+++ lab11/lab11_z5/lab11_z5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11_z5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, int> method = Do;
+            IAsyncResult cookie = method.BeginInvoke("test", null, null);
+            // .. tutaj możemy równolegle wykonywać inne prace ...
+            int result = method.EndInvoke(cookie);
+            Console.WriteLine("String length is: " + result);
+        }
+        static int Do(string s) { return s.Length; }
+    }
+}
