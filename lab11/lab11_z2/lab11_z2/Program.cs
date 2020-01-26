ef3e70f6cadd08939a4e1d0164778c7d2ffce62d
murmiloo <kkaplyta@wp.pl>
2020-01-26 22:34:14 +0100

    Create Program.cs

---
+++ lab11/lab11_z2/lab11_z2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab11_z2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            new Thread(Run).Start();
+        }
+        static void Run()
+        {
+            try
+            {
+                // ...
+                throw null;
+                // ...
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!");
+            }
+        }
+    }
+}
